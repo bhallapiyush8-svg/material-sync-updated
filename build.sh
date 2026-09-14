4cb1d0589576549e4c266a1cc8f8254b76d6a230
Piyush Bhalla <bhallapiyush8@gmail.com>
2026-09-14 18:53:48 +0530

    Fix build.sh to install requirements before running migrations

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py migrate
-
 python manage.py collectstatic --noinput
-
-gunicorn config.wsgi:application
